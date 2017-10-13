45241ab06c1e3bba8f551acf6fe6397eeda0a689
GroverTB <grover.jtb@gmail.com>
2017-10-13 12:21:04 -0500

    [21] Crear cliente para consumir el API

---
+++ source/api.js
@@ -0,0 +1,26 @@
+const baseUrl = "http://jsonplaceholder.typicode.com"
+
+const api = {
+  posts: {
+    async getList(page = 1) {
+      const response = await fetch(`${baseUrl}/posts?_page=${page}`)
+      return await response.json()
+    },
+    async getSingle(id = 1) {
+      const response = await fetch(`${baseUrl}/posts/${id}`)
+      return await response.json()
+    },
+    async getComment(id = 1) {
+      const response = await fetch(`${baseUrl}/${id}/comments`)
+      return await response.json()
+    }
+  },
+  users: {
+    async getSingle(id = 1) {
+      const response = await fetch(`${baseUrl}/users/${id}`)
+      return await response.json()
+    }
+  }
+}
+
+export default api
